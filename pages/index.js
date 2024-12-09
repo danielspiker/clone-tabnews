bf0ac17184fffbc580fd71ead47e50a6e6af38bb
Daniel Marques do Nascimento <danielmnascimento@gmail.com>
2024-12-09 19:22:51 +0000

    bold love

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mo, amo você. Se tu me amas, dá uma risadinha ;D</h1>
+    return <h1>Mo, <strong>amo</strong> você. Se tu me amas, dá uma risadinha ;D</h1>
 }
 
 export default Home;
