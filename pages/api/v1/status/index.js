526e7947d30861f5920c93b0f78dd1dec034a057
Daniel Marques do Nascimento <danielmnascimento@gmail.com>
2025-01-25 14:09:35 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,34 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
+  //controller
+  const updatedAt = new Date().toISOString();
 
-  console.log(">>>>>", result.rows);
-  response.status(200).json({ frase: "sou acima da média!" });
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnections = databaseMaxConnResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnValue = usedConnResult.rows[0].count;
+
+  //retorna pra view
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        used_conn: databaseOpenedConnValue,
+      },
+    },
+  });
 }
 
 export default status;
